ed504ea3efd811e0e298fa5cf7a5fc446ebb7fbf
Shekhar Verma <shekharverma2107@gmail.com>
2024-10-16 11:04:22 +0530

    Created one directory inside the root directory and make changes inside it

---
+++ Project/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is a home page</title>
+    </head>
+    <body>
+        <center>
+            <h1>Welcome to the home page</h1>
+            <p>If you are not already a user then, <a href="register.php">register here</a></p>
+            <p>If you are already a user then, <a href="login.php">login here</a></p>
+        </center>
+    </body>
+</html>
